55741fbd0b62f2926f32badeb4016b1942225f00
georgiev-ventsi <v.g.al.hq@gmail.com>
2020-07-08 22:45:29 +0300

    move the products from static to dynamic state

--- react-node-mongodb-sql_ecommerce/frontend/src/App.jsx
+++ react-node-mongodb-sql_ecommerce/frontend/src/App.jsx
@@ -4,106 +4,67 @@ import './App.css';
 
 function App() {
 
-        const openMenu = () => {
-            document.querySelector(".sidebar").classList.add("open")
-        }
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open")
+    }
 
-        const closeMenu = () => {
-            document.querySelector(".sidebar").classList.remove("open")
-        }
+    const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open")
+    }
 
-        return (
-            <div className="grid-container">
+    return (
+        <div className="grid-container">
             <header className="header">
-              <div className="brand">
-                  <button className="brand-button" onClick={openMenu}>
-                      {/* <!-- ASCII hamburger menu --> */}
+                <div className="brand">
+                    <button className="brand-button" onClick={openMenu}>
+                        {/* <!-- ASCII hamburger menu --> */}
                       &#9776;
                   </button>
-                  <a href="./index.html">Husqvarna</a>
-              </div>
-              <div className="header-links">
-                  <a href="cart.html">Количка</a>
-                  <a href="signin.html">ВЛЕЗ</a>
-              </div>
-          </header>
-          <aside className="sidebar">
-              <h3 class="categories-tytle">Категории</h3>
-              <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-              <ul>
-                  <li>
-                      <a href="index.html">Косачки роботи</a>
-                  </li>
-                  <li>
-                      <a href="index.html">Бензинови триони</a>
-                  </li>
-              </ul>
-          </aside>
-          <main className="main">
-              <div className="content">
-                      <ul className="products">
-                          <li>
-                              <div className="product">
-                                  <img className="product-image" src="./images/am105.png" alt="automower 105" />
-                                  <div className="product-name"><a href="product.html">Automower 105</a></div>
-                                  <div className="product-brand">Husqvarna</div>
-                                  <div className="product-price">2540,00 лв.</div>
-                                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                              </div>
-                          </li>
-                          <li>
-                              <div className="product">
-                                  <img className="product-image" src="./images/am105.png" alt="automower 105" />
-                                  <div className="product-name"><a href="product.html">Automower 105</a></div>
-                                  <div className="product-brand">Husqvarna</div>
-                                  <div className="product-price">2540,00 лв.</div>
-                                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                              </div>
-                          </li>
-                          <li>
-                              <div className="product">
-                                  <img className="product-image" src="./images/am105.png" alt="automower 105" />
-                                  <div className="product-name"><a href="product.html">Automower 105</a></div>
-                                  <div className="product-brand">Husqvarna</div>
-                                  <div className="product-price">2540,00 лв.</div>
-                                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                              </div>
-                          </li>
-                          <li>
-                              <div className="product">
-                                  <img className="product-image" src="./images/am105.png" alt="automower 105" />
-                                  <div className="product-name"><a href="product.html">Automower 105</a></div>
-                                  <div className="product-brand">Husqvarna</div>
-                                  <div className="product-price">2540,00 лв.</div>
-                                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                              </div>
-                          </li>
-                          <li>
-                              <div className="product">
-                                  <img className="product-image" src="./images/am105.png" alt="automower 105" />
-                                  <div className="product-name"><a href="product.html">Automower 105</a></div>
-                                  <div className="product-brand">Husqvarna</div>
-                                  <div className="product-price">2540,00 лв.</div>
-                                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                              </div>
-                          </li>
-                          <li>
-                              <div className="product">
-                                  <img className="product-image" src="./images/am105.png" alt="automower 105" />
-                                  <div className="product-name"><a href="product.html">Automower 105</a></div>
-                                  <div className="product-brand">Husqvarna</div>
-                                  <div className="product-price">2540,00 лв.</div>
-                                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                              </div>
-                          </li>
-                      </ul>
-                  </div>
-          </main>
-          <footer className="footer">
-              All rights reserved.
+                    <a href="./index.html">Husqvarna</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Количка</a>
+                    <a href="signin.html">ВЛЕЗ</a>
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3 class="categories-tytle">Категории</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul>
+                    <li>
+                        <a href="index.html">Косачки роботи</a>
+                    </li>
+                    <li>
+                        <a href="index.html">Бензинови триони</a>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <ul className="products">
+                        {
+                            data.products.map(product => 
+                                <li>
+                                    <div className="product">
+                                        <img className="product-image" src={product.image} alt="automower 105" />
+                                        <div className="product-name">
+                                            <a href="product.html">"{product.name}</a>
+                                        </div>
+                                        <div className="product-brand">{product.brand}</div>
+                                        <div className="product-price">{product.price}</div>
+                                        <div className="product-rating">{product.rating} Stars({product.numReviews})</div>
+                                    </div>
+                                </li>
+                                )
+                        }
+                    </ul>
+                </div>
+            </main>
+            <footer className="footer">
+                All rights reserved.
           </footer>
-      </div>
-        );
+        </div>
+    );
 
 }
 
