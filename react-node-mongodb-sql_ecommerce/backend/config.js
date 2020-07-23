19af245cbda7e14111abffa51ccd257efad3cb33
georgiev-ventsi <v.g.al.hq@gmail.com>
2020-07-23 07:21:48 +0300

    add signin logic

--- react-node-mongodb-sql_ecommerce/backend/config.js
+++ react-node-mongodb-sql_ecommerce/backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/react-node-mongodb-sql_ecommerce'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/react-node-mongodb-sql_ecommerce',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
